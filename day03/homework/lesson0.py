97b61a381bba065a63c8f2fb359fc6903ae88d14
mateshamatava <kunichuk74@mail.ru>
2025-01-24 20:52:33 +0200

    commit

---
+++ day03/homework/lesson0.py
@@ -0,0 +1,34 @@
+# age = 50$  cvlads vadzlevt saxels da shemdeg toloba da shemdeg vadzlevt mnishvnelobasm
+
+num1 = 1 
+num2 = 2 
+num3 = 3  
+print(num1+num2+num3) 
+
+x = 5 
+y = 2 
+
+print(x*y) 
+ 
+length = 20 
+width = 10 
+
+print((length +  width)*2) 
+
+age1 = 30 
+age2 = 35 
+age3 = 20 
+
+print((age1+age2+age3)/3)
+
+name = "mate" 
+lastname ="shamatava" 
+result =name + " " + lastname
+print(result) 
+ 
+fullname = result+" "
+age = 12  
+print(fullname * age ) 
+
+# deklaracia ari roca magalitad age  mivcemt monacemta tips integers an name mivcemt strings 
+# inicializacia aris shekmna da reincializacia aris tavidan shekmna
